8ab7f3304dda271bc6f9d13c0be2c65b071e6dcd
Libin's sec <li296641798@gmail.com>
2020-05-07 19:21:12 +0800

    [update]..

--- CustomGraphicsView.py
+++ CustomGraphicsView.py
@@ -12,23 +12,26 @@ class CustomGraphicsView(QtWidgets.QGraphicsView):
         self.setMouseTracking(0)
         self.setTransformationAnchor(self.NoAnchor)
         self.keyCtrlOn = 0
+        self.mousePressOn = 0
 
     def mousePressEvent(self, event: QtGui.QMouseEvent):
         print("mousePressEvent in")
         self.mousePressPos = event.globalPos()
         self.mouseMovePos1 = event.globalPos()
+        self.mousePressOn = 1
 
     def mouseReleaseEvent(self, event: QtGui.QMouseEvent):
         print("mouseReleaseEvent in")
-        #self.setTransformationAnchor(self.NoAnchor)
+        self.mousePressOn = 0
 
     def mouseMoveEvent(self, event: QtGui.QMouseEvent):
         print("mouseMoveEvent in")
-        self.mouseMovePos0 = self.mouseMovePos1
-        self.mouseMovePos1 = event.globalPos()
-        temp = self.mouseMovePos1 - self.mouseMovePos0
-        self.translate(temp.x(), temp.y())
-        self.show()
+        if self.mousePressOn:
+            self.mouseMovePos0 = self.mouseMovePos1
+            self.mouseMovePos1 = event.globalPos()
+            temp = self.mouseMovePos1 - self.mouseMovePos0
+            self.translate(temp.x(), temp.y())
+            self.show()
 
     def dragMoveEvent(self, event: QtGui.QDragMoveEvent):
         print("dragMoveEvent in")
@@ -39,18 +42,15 @@ class CustomGraphicsView(QtWidgets.QGraphicsView):
         angle = event.angleDelta()
 
         if self.keyCtrlOn:
-        #if 1:
-            #self.setTransformationAnchor(self.AnchorUnderMouse)
+            self.setTransformationAnchor(self.AnchorUnderMouse)
             if angle.y() > 0:
-                self.setTransformationAnchor(self.AnchorUnderMouse)
                 self.scale(1.1, 1.1)
-                self.setTransformationAnchor(self.NoAnchor)
+                self.show()
             else:
-                self.setTransformationAnchor(self.AnchorUnderMouse)
                 self.scale(0.9, 0.9)
-                self.setTransformationAnchor(self.NoAnchor)
-            #self.setTransformationAnchor(self.NoAnchor)
-            self.show()
+                self.show()
+            self.setTransformationAnchor(self.NoAnchor)
+            #self.show()
 
     def keyPressEvent(self, event: QtGui.QKeyEvent):
         print("keyPressEvnet in")
