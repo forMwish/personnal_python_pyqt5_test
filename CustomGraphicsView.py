c25dfa41661c42bd4af7cb4c6b87cc3a351797d1
muziwenwu <li296641798@gmail.com>
2020-05-08 00:14:54 +0800

    [update]..

--- CustomGraphicsView.py
+++ CustomGraphicsView.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 
 class CustomGraphicsView(QtWidgets.QGraphicsView):
@@ -6,11 +6,12 @@ class CustomGraphicsView(QtWidgets.QGraphicsView):
         super(CustomGraphicsView, self).__init__(*__args)
         self.pointMode = 0
         self.rectMode = 0
-        self.mousePressPos = [0, 0]
-        self.mouseMovePos0 = [0, 0]
-        self.mouseMovePos1 = [0, 0]
-        self.setMouseTracking(0)
+        self.mousePressPos = QtCore.QPoint()
+        self.mouseMovePos0 = QtCore.QPoint()
+        self.mouseMovePos1 = QtCore.QPoint()
+        self.setMouseTracking(1)
         self.setTransformationAnchor(self.NoAnchor)
+
         self.keyCtrlOn = 0
         self.mousePressOn = 0
 
@@ -26,31 +27,32 @@ class CustomGraphicsView(QtWidgets.QGraphicsView):
 
     def mouseMoveEvent(self, event: QtGui.QMouseEvent):
         print("mouseMoveEvent in")
+        self.mouseMovePos0 = self.mouseMovePos1
+        self.mouseMovePos1 = event.globalPos()
         if self.mousePressOn:
-            self.mouseMovePos0 = self.mouseMovePos1
-            self.mouseMovePos1 = event.globalPos()
             temp = self.mouseMovePos1 - self.mouseMovePos0
             self.translate(temp.x(), temp.y())
             self.show()
 
     def dragMoveEvent(self, event: QtGui.QDragMoveEvent):
         print("dragMoveEvent in")
-        pass
 
     def wheelEvent(self, event: QtGui.QWheelEvent):
         print("wheelEvnet in")
-        angle = event.angleDelta()
+
+        oldPos = self.mapToScene(event.pos())
 
         if self.keyCtrlOn:
-            self.setTransformationAnchor(self.AnchorUnderMouse)
+            angle = event.angleDelta()
             if angle.y() > 0:
                 self.scale(1.1, 1.1)
-                self.show()
             else:
                 self.scale(0.9, 0.9)
-                self.show()
-            self.setTransformationAnchor(self.NoAnchor)
-            #self.show()
+
+        newPos = self.mapToScene(event.pos())
+
+        deltaPos = newPos - oldPos
+        self.translate(deltaPos.x(), deltaPos.y())
 
     def keyPressEvent(self, event: QtGui.QKeyEvent):
         print("keyPressEvnet in")
--- main.py
+++ main.py
@@ -18,40 +18,24 @@ class MainWin(mw.Ui_MainWindow):
         self.scaleX = 1
         self.scaleY = 1
 
-
     def loadPic(self, widget):
         print("[test] class:MainWin -> fun:loadPic -> In ")
-        file_path = QtWidgets.QFileDialog.getOpenFileName(widget, "选择加载图片", "./resource/picture/")
+        file_path = QtWidgets.QFileDialog.getOpenFileName(widget, "选择加载图片", "./resource/picture/", "图片 (*.jpg)")
+
         print("[test] picture path: ", file_path[0])
 
         pix = QtGui.QPixmap(file_path[0])
-        #rect = QtWidgets.QGraphicsRectItem(0, 0, 100, 100)
+        # rect = QtWidgets.QGraphicsRectItem(0, 0, 100, 100)
 
         self.scene.addPixmap(pix)
-        #self.scene.addItem(rect)
+        # self.scene.addItem(rect)
 
         self.graphicsView.setScene(self.scene)
-        self.graphicsView.show()
-
-    def zoomInPic(self):
-        print("[test] class:MainWin -> fun:zoomInPic -> In ")
-        self.scaleX = 1.1
-        self.scaleY = 1.1
-        self.graphicsView.scale(self.scaleX, self.scaleY)
-        self.graphicsView.show()
-
-    def zoomOutPic(self):
-        print("[test] class:MainWin -> fun:zoomOutPic -> In ")
-        self.scaleX = 0.9
-        self.scaleY = 0.9
-        self.graphicsView.scale(self.scaleX, self.scaleY)
         self.graphicsView.show()
 
     def connect(self, widget):
         print("[test] class:MainWin -> fun:connect -> In ")
         self.pushButton_loadPicture.clicked.connect(lambda: self.loadPic(widget))
-        self.zoomIn.triggered.connect(lambda: self.zoomInPic())
-        self.zoomOut.triggered.connect(lambda: self.zoomOutPic())
 
 
 if __name__ == "__main__":
